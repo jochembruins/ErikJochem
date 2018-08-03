2577545db1518c940dcce051578756ea6fd96a11
Jochem Bruins <jochembruins@gmail.com>
2018-08-03 12:03:33 +0200

    add tweetscraper

---
+++ tweetscraper.py
@@ -0,0 +1,8 @@
+import tweepy
+from tweepy import OAuthHandler
+
+ 
+consumer_key = 'YOUR-CONSUMER-KEY'
+consumer_secret = 'YOUR-CONSUMER-SECRET'
+access_token = 'YOUR-ACCESS-TOKEN'
+access_secret = 'YOUR-ACCESS-SECRET'
