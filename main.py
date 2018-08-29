cbedecc3f93ba10247fd955f558cd6e064072e35
Erik van Zummeren <erikvanzummeren@gmail.com>
2018-08-29 14:26:49 +0200

    updated list

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from tweetscraper import getTweets
 import json
 
-with open('TweedeKamerScraper/leden.json', 'r') as json_file:
+with open('TweedeKamerScraper/leden_complete.json', 'r') as json_file:
 	data = json.load(json_file)
-	print(data[0]['twitter'])
 
 for members in data:
 	if 'twitter' in members:
