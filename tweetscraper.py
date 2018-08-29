e7cf076e10f2843346b950f109614547eb8b5a75
Erik van Zummeren <erikvanzummeren@gmail.com>
2018-08-29 11:26:39 +0200

    tweets

--- tweetscraper.py
+++ tweetscraper.py
@@ -1,8 +1,14 @@
-import tweepy
-from tweepy import OAuthHandler
+from TwitterAPI import TwitterAPI;
+import json;
 
- 
-consumer_key = 'YOUR-CONSUMER-KEY'
-consumer_secret = 'YOUR-CONSUMER-SECRET'
-access_token = 'YOUR-ACCESS-TOKEN'
-access_secret = 'YOUR-ACCESS-SECRET'
+SEARCH_TERM = 'geertwilderspvv'
+
+r = api.request('statuses/user_timeline', {'screen_name': SEARCH_TERM, 'count': 3200})
+
+with open('geert.json', 'w') as outfile
+
+for item in r:
+    json.dump(item if 'text' in item else item, outfile)
+    // print(item if 'text' in item else item)
+
+print('\nQUOTA: %s' % r.get_quota())
